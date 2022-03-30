88cbcb86594034255414b433251869f1fc8af556
DESKTOP-05DD7JP\veron <tomashudecek04@gmail.com>
2022-03-30 17:03:03 +0100

    Initial commit

---
+++ shop/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import {Recipe} from "../recipe.model";
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('A test Recipe', 'this is simply test', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=700,636'),
+    new Recipe('A test Recipe', 'this is simply test', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=700,636')
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
